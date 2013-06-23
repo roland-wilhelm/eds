da1787690c19f645691495e37449ea62fac581c6
Benedikt Kleinmeier <kleinmei@hm.edu>
2013-06-23 16:13:51 +0200

    Added first version of active object "coffee machine" as "coffeemachine_ao.c".
    The active object has two states:
    - Idle
    - Brewing
    
    The following transitions are possible:
    
                   Alarm (RTC)
         +------+ -------------> +---------+
    o--> | Idle |                | Brewing |
         +------+ <------------- +---------+
                    ShortPress
    
    At the moment, the user doesn't get a visual feedback when in "Brewing" state. Maybe, the LEDs should flash.

---
+++ FreeRTOS_AOs/rtos/rtosbase/src/app/coffeemachine_ao.h
@@ -0,0 +1,32 @@
+/**
+ * coffeemachine_ao.h
+ *
+ * benedikt kleinmeier
+ * june 23, 2013
+ */
+ 
+ #ifndef COFFEE_MACHINE_H_
+ #define COFFEE_MACHINE_H_
+ 
+ #include "qf_pkg.h"
+ #include "events.h"
+ 
+ // make opaque pointers available externally
+ extern QActive* const CoffeeMachineAOBase;
+ extern QEvent const *l_CoffeeMachineAOEvtQSto[SIZE_OF_EVENT_QUEUE];
+ 
+ // CoffeeMachineAO active object
+ typedef struct CoffeeMachineAOTag {
+	 QActive super;
+ } CoffeeMachineAO;
+ 
+ // constructor
+ void CoffeeMachineAO_ctor(void);
+ 
+ // state handlers
+ static QState CoffeeMachineAO_initial(CoffeeMachineAO *me, QEvent const *e);
+ static QState CoffeeMachineAO_Idle(CoffeeMachineAO *me, QEvent const *e);
+ static QState CoffeeMachineAO_Brewing(CoffeeMachineAO *me, QEvent const *e);
+ 
+ #endif // COFFEE_MACHINE_H_
+ 
